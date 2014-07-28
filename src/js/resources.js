359fc3ed9dfd731ab74f61c060579c82a2acc332
Joe Mattelaer <josephmattelaer@gmail.com>
2014-07-28 19:08:19 +0100

    Init
    General Init

---
+++ src/js/resources.js
@@ -0,0 +1,31 @@
+//game resources
+game.resources = [
+    /**
+     * Graphics.
+     */
+    // our level tileset
+    {name: "gfxlib_FreeTileset_64",  type:"image", src: "data/img/map/gfxlib_FreeTileset_64.png"},
+
+    // our metatiles
+    {name: "gfxlib_FreeTileset_64",  type:"image", src: "data/img/map/gfxlib_FreeTileset_64.png"},
+
+    // main player spritesheet
+    {name: "gripe_run_right",  type:"image", src: "data/img/sprite/gripe_run_right.png"},
+
+    // NPC textures
+    {name: "npc_fred",  type:"image", src: "data/img/entity/npc/fred.png"},
+    {name: "npc_red_invader",  type:"image", src: "data/img/entity/npc/red_invader.png"},
+
+    // particle textures
+    {name: "particle_explosion",  type:"image", src: "data/img/entity/particle/explosion.png"},
+    {name: "particle_smoke",  type:"image", src: "data/img/entity/particle/smoke.png"},
+
+    // fonts
+    {name: "32x32_font",  type:"image", src: "data/img/font/32x32_font.png"},
+     
+    /* 
+     * Maps. 
+     */
+    {name: "001_pilot", type: "tmx", src: "data/map/001_pilot.tmx"}
+ 
+];
