8ab53d9d97620a79ac78ee034151831eb71136c2
Glovers <elevatedlegendz@gmail.com>
2014-07-30 22:16:50 +0100

    Made template for Hunter.js

--- src/js/entities/NPCs/Hunter.js
+++ src/js/entities/NPCs/Hunter.js
@@ -1,55 +1,90 @@
+
 game.Hunter = me.ObjectEntity.extend({
-    
-        /* -----
- 
-    constructor
- 
-    ------ */
- 
-    init: function(x, y, settings) {
-        // call the constructor
-        this.parent(x, y, settings);
-
-        // This variable allows us to set a type for the player so we can compare it later.
-        // This is useful for collisions, we can check to see if an NPC has collided with the player.
-        this.type = "hunter"
- 
-        // set the default horizontal & vertical speed (accel vector)
-        this.setVelocity(5, 5);
-
-        // ptthh. Who needs gravity?
-        this.gravity = 0
-
-        this.collidable = true;
-        
-        this.health = 10;
-
-        this.maxHealth = 10;
-
-        this.minHealth = 0;
+	"init": function(x, y, settings) {
 
 
-    },
-    
-//get player entity
-        var player = me.game.getEntityByName("mainPlayer")[0];
+  	console.log("Hunter init called"); // called
 
-        //create vector based on player's postion
-        var xDir = player.pos.x - this.pos.x;
-        var yDir = player.pos.y - this.pos.y;
-        console.log(xDir);
+  	this.parent(x, y, settings);
 
-        //Decide distance
+  	this.type = "Hunter"
 
-        xDir = (Math.abs(xDir) < 8) ? 0 : xDir.clamp(-1,1);
-        yDir = (Math.abs(yDir) < 8) ? 0 : yDir.clamp(-1,1);
+    this.health = 10;
 
-        //console.log(xDir);
+  	this.collidable = true;
 
-        this.vel.x += this.accel.x * xDir;
-        this.vel.y += this.accel.y * yDir;
+    this.canHit = true;
 
-        this.updateMovement();
+    this.renderable.addAnimation("debug_die", [ 0, 1, 2, 3, 4], 200);
+
+    //this.renderable.setCurrentAnimation("debug_die");
+
+
+    //this.renderable.setCurrentAnimation("debug_die");
+
+  },
+
+  update: function(dt) {
+
+    console.log("dun");
+
+    player = me.game.world.getChildByName("player")[0];
+
+
+    // Use pythag to check if the enemy is closer than 96 units
+    if(Math.sqrt(Math.abs(Math.pow(this.pos.x - player.pos.x, 2)) + Math.abs(Math.pow(this.pos.y - player.pos.y, 2))) < 96){
+
+      if(this.canHit){    
+        player.hit(1);
+
+        this.canHit = false;
+      }
+
+    }else{
+      console.log("Can Print!");
+      this.canHit = true;
+    }
+
+  },
+
+  onCollision: function(res, obj){
+
+  	if (obj.type === 'player'){
+  		// Do something?
+  	}
+
+  },
+
+  hit: function(damage){
+
+    this.health -= damage;
+
+    if(this.health <= 0){
+      console.log("deaded");
+      //dead!
+      //this.destroy();
+
+      //this.renderable.setCurrentAnimation("debug_die");
+
+      //this.renderable.setAnimationFrame();
+
+      if (!this.renderable.isCurrentAnimation("debug_die")) {
+
+        console.log("not debug_dieing")
+        // do something funny...
+        var _this = this;
+        this.renderable.setCurrentAnimation("debug_die", function () {
+          me.game.world.removeChild(_this);
+        });
+      }
+
+
+
+
+    }else{
+      console.log("HIT!");
+    }
+  }
 
 
 });
