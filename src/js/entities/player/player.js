b5ac5b82c00dc85f4afe16b5d87a96a0d6e2a0e7
Glovers <elevatedlegendz@gmail.com>
2014-08-01 09:22:33 +0100

    N/A

--- src/js/entities/player/player.js
+++ src/js/entities/player/player.js
@@ -22,7 +22,12 @@ game.PlayerEntity = me.ObjectEntity.extend({
  
         // set the default horizontal & vertical speed (accel vector)
         this.setVelocity(5, 5);
+        
+        this.renderable.addAnimation("debug_die", [1, 2, 3, 4], 200);
 
+        this.renderable.addAnimation("idle", [1], 100);
+
+        this.renderable.setCurrentAnimation("idle");
         // ptthh. Who needs gravity?
         this.gravity = 0
 
