813fdc1efbf738dbbb229e6a6ae65fca413aeeba
JamesMuir <JRMuir09@gmail.com>
2014-09-30 20:46:22 +0100

    Fixed bug
    Burglar texture bug fix.

--- src/js/entities/enemies/Burglar.js
+++ src/js/entities/enemies/Burglar.js
@@ -13,12 +13,6 @@ game.burglar = game.Criminal.extend({
 
     this.health = 15;
 
-    this.renderable.addAnimation("debug_die_anim", [0, 1, 2, 3, 4], 200);
-
-    this.renderable.addAnimation("idle", [0], 100);
-
-    this.renderable.setCurrentAnimation("idle");
-
 
   }
 
